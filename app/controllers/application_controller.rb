63774d84770ea781425e030c92e830c746fbf398
Joseph <joffan98@gmail.com>
2022-11-15 11:31:49 -0600

    commit tree a00f66458857fea7ebd1d4db4ced0647094d9a73

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,22 @@
 class ApplicationController < Sinatra::Base
 
+  set :default_content_type, 'application/json'
+  
   get '/' do
     { message: "Hello world" }.to_json
+  end
+
+  get '/games' do
+    Game.all.limit(10).to_json
+  end
+
+  get '/games/:id' do
+    Game.find(params[:id]).to_json(
+      only: [:id, :title, :genre, :price], include: {
+      reviews: { only: [:comment, :score], include: {
+        user: { only: [:name] }
+      } }
+    })
   end
 
 end
